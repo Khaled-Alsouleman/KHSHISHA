62d37715bb5152408bde8a1c73d3689945d65d72
Khaled AlSouleman <khaled.alsouleman@prismat.de>
2025-09-06 15:05:54 +0200

    Deploy Shooters screen

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2773882123"
+    serviceWorkerVersion: "2900628672"
   }
 });
