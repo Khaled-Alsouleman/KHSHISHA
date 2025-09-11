448212405a737e02ee65a86e99829774b315fccf
Khaled AlSouleman <khaled.alsouleman@prismat.de>
2025-09-11 17:00:45 +0200

    Deploy opening times to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2071491023"
+    serviceWorkerVersion: "4108040857"
   }
 });
