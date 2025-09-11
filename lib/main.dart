6aa089829a914bea4afef3b05158ccf2239b92c9
Khaled AlSouleman <khaled.alsouleman@prismat.de>
2025-09-11 16:59:22 +0200

    opening times

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   ResponsiveSizingConfig.instance.setCustomBreakpoints(
     const ScreenBreakpoints(desktop: 900, tablet: 600, watch: 250),
   );
